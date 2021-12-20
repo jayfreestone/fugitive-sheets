601e1d240bac05bdfa326cf51f3d9caa308d26d4
Jay Freestone <mail@jayfreestone.com>
2021-12-20 13:02:02 +0000

    Add poor man's caching to getSheetIds

--- app/prose/getSheetIDs.ts
+++ app/prose/getSheetIDs.ts
@@ -1,9 +1,17 @@
 import fs from 'fs';
 import path from 'path';
 
+// Would usually be smarter about this and leave caching up to
+// the root/entry point, but not worth the effort for this.
+let ids: string[];
+
 function getSheetIDs() {
-  const files = fs.readdirSync('./sheets');
-  return files.map((filename) => path.parse(filename).name);
+  if (!ids) {
+    const files = fs.readdirSync('./sheets');
+    ids = files.map((filename) => path.parse(filename).name);
+  }
+
+  return ids;
 }
 
 export default getSheetIDs;
